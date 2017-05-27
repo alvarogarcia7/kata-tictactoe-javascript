43a6681f846ba6e98b87b8c12599473ffdbc3e6a
Alvaro Garcia <derecerca@gmail.com>
2017-05-27 11:05:04 +0200

    Refactor: extract class Store to its own file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Store from './Store' 
 
 class App extends Component {
 
@@ -22,43 +23,6 @@ class App extends Component {
     return (
         <li>winner is </li>
     )
-  }
-}
-
-class Store {
-  constructor() {
-    this.channels = {}
-  }
-
-  upsertChannel(name){
-    if(!this.channels[name]){
-      this.channels[name] = new Channel()
-    }
-    return this.channels[name]
-  }
-
-  publish(channelName, message) {
-    this.upsertChannel(channelName).publish(message)
-  }
-
-  subscribe(channelName, function_) {
-    this.upsertChannel(channelName).addSubscriber(function_)
-  }
-}
-
-class Channel {
-  constructor() {
-    this.history = []
-    this.subscribers = []
-  }
-
-  addSubscriber(function_) {
-    this.subscribers.push(function_)
-  }
-
-  publish(message) {
-    this.history.push(message)
-    this.subscribers.map(f => f(this.history))
   }
 }
 
