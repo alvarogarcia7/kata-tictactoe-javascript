0ae343a4bb2d09a5be1c189e42d975a78372dbef
Alvaro Garcia <derecerca@gmail.com>
2017-05-27 11:10:33 +0200

    Refactor: extract class Board, Square to its own file

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Store from './Store' 
+import { Board } from './Board'
 
 class App extends Component {
 
@@ -22,90 +23,6 @@ class App extends Component {
   winner() {
     return (
         <li>winner is </li>
-    )
-  }
-}
-
-class Board extends Component {
-
-  constructor() {
-    super()
-    this.initialState = {
-      board: ['','','',
-              '','','',
-              '','',''],
-      player: 'X',
-    }
-    this.state = {
-      moves: [],
-      board: [],
-    }
-  }
-
-  componentWillMount(){
-    this.props.store.subscribe('makeamove', this.replay.bind(this))
-    this.replay(this.state.moves)
-  }
-
-  render() {
-    return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
-  }
-
-  renderSquare(index) {
-    return (
-      <Square 
-        value={this.state.board[index]} 
-        onClick={() => {makeAMove.bind(this)(index)}}
-      />
-    )
-
-    function makeAMove(index) {
-      this.props.store.publish('makeamove', {type: 'put', at: index}); 
-    }
-  }
-
-  replay(moves) {
-    var currentPlayer = this.initialState.player
-    var board = shallowClone(this.initialState.board)
-    moves.map(move => {
-      let index = move.at;
-      if (board[index] === '') {
-        board[index] = currentPlayer
-        currentPlayer = (currentPlayer === 'X') ? 'O' : 'X'
-      }
-    })
-    this.setState({board: board})
-
-    function shallowClone(obj) {
-      return Object.assign({}, obj);
-    }
-  }
-}
-
-class Square extends Component {
-  render() {
-    return (
-    <button className="square" onClick={this.props.onClick}>
-       {this.props.value}
-    </button>
     )
   }
 }
---
+++ src/Board.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Board extends Component {
+
+  constructor() {
+    super()
+    this.initialState = {
+      board: ['','','',
+              '','','',
+              '','',''],
+      player: 'X',
+    }
+    this.state = {
+      moves: [],
+      board: [],
+    }
+  }
+
+  componentWillMount(){
+    this.props.store.subscribe('makeamove', this.replay.bind(this))
+    this.replay(this.state.moves)
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+
+  renderSquare(index) {
+    return (
+      <Square 
+        value={this.state.board[index]} 
+        onClick={() => {makeAMove.bind(this)(index)}}
+      />
+    )
+
+    function makeAMove(index) {
+      this.props.store.publish('makeamove', {type: 'put', at: index}); 
+    }
+  }
+
+  replay(moves) {
+    var currentPlayer = this.initialState.player
+    var board = shallowClone(this.initialState.board)
+    moves.map(move => {
+      let index = move.at;
+      if (board[index] === '') {
+        board[index] = currentPlayer
+        currentPlayer = (currentPlayer === 'X') ? 'O' : 'X'
+      }
+    })
+    this.setState({board: board})
+
+    function shallowClone(obj) {
+      return Object.assign({}, obj);
+    }
+  }
+}
+
+class Square extends Component {
+  render() {
+    return (
+    <button className="square" onClick={this.props.onClick}>
+       {this.props.value}
+    </button>
+    )
+  }
+}
+
+export { Board };
