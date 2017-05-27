066833c3bf3e749756b0f9353fc7117dbd9fd988
Alvaro Garcia <derecerca@gmail.com>
2017-05-27 11:26:38 +0200

    Create the full board

--- src/App.js
+++ src/App.js
@@ -25,17 +25,31 @@ class Board extends Component {
   constructor() {
     super()
     this.state = {
-      board: ['','',''],
+      board: ['','','',
+              '','','',
+              '','',''],
       currentPlayer: 'X',
     }
   }
 
   render() {
     return (
-      <div className="board-row">
-        {this.renderSquare(0)}
-        {this.renderSquare(1)}
-        {this.renderSquare(2)}
+      <div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
       </div>
     );
   }
