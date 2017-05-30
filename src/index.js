a953f2bf05ffda85c62342e815b63d7116e91efd
Alvaro Garcia <derecerca@gmail.com>
2017-05-30 23:52:47 +0200

    Change to HashRouter

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import FullApp from './FullApp'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
 ReactDOM.render((
-  <BrowserRouter>      
+  <HashRouter history={History}>      
     <FullApp /> 
-  </BrowserRouter>),
+  </HashRouter>),
   document.getElementById('root'));
 registerServiceWorker();
